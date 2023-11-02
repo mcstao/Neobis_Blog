6f4243dbb177fed72f9907f19b80bdb0f1b53dc9
mcstao <arislg99@gmail.com>
2023-11-02 12:59:59 +0600

    view home page

--- post_app/views.py
+++ post_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from post_app.models import Post
+
+
+def home(request):
+    if request.method == "GET":
+        posts = Post.objects.all()
+        context = {
+            'posts': posts
+        }
+        return render(request, 'posts/home.html', context)
+
