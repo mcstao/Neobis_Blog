bc1b96dbf9babf9b1dade9e59d3d6a64a6a20721
mcstao <arislg99@gmail.com>
2023-11-04 13:14:22 +0600

    New function for post(post_date)

--- post_app/models.py
+++ post_app/models.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 class Post(models.Model):
     title = models.CharField(max_length=100, verbose_name="Название")
     description = models.TextField(max_length=500, verbose_name="Описание")
     image = models.ImageField(verbose_name="Фото")
     # author = models.ForeignKey(User, on_delete=models.CASCADE)
-    # post_date
+    post_date = models.DateTimeField(default=timezone.now(), verbose_name="Время публикации")
