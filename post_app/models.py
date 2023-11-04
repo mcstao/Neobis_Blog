489f481e9b0f5bce9257a415a9fe28625e1a961e
mcstao <arislg99@gmail.com>
2023-11-04 21:05:23 +0600

    Created model author

--- post_app/models.py
+++ post_app/models.py
@@ -6,5 +6,5 @@ class Post(models.Model):
     title = models.CharField(max_length=100, verbose_name="Название")
     description = models.TextField(max_length=500, verbose_name="Описание")
     image = models.ImageField(verbose_name="Фото")
-    # author = models.ForeignKey(User, on_delete=models.CASCADE)
-    post_date = models.DateTimeField(default=timezone.now(), verbose_name="Время публикации")
+    post_date = models.DateTimeField(default=timezone.now, verbose_name="Время публикации")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
